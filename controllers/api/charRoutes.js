7f17afa5219228070aeaef39a5b70fe3d3952fe2
Hunter Padgett <gizmokabanjo@gmail.com>
2022-08-03 16:56:00 -0400

    plethora

--- controllers/api/charRoutes.js
+++ controllers/api/charRoutes.js
@@ -1,4 +1,65 @@
 const router = require('express').Router();
 const { User, Fighter } = require('../../models');
 
+// api/fighter
+
+router.get('/', async (req, res) => {
+  // find all categories ✅
+  // be sure to include its associated Products ✅
+  try {
+    const fighterData = await Fighter.findAll({
+      include: User
+    });
+    res.status(200).json(fighterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value ✅
+  // be sure to include its associated Products ✅
+  try {
+    const fighterData = await Fighter.findByPk(req.params.id, {
+      include: Product
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'no fighter found with that id'});
+      return;
+    }
+
+    res.status(200).json(fighterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const fighterData = await Fighter.create(req.body);
+    res.status(200).json(fighterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const fighterData = await Fighter.findByPk(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!fighterData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(fighterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,19 @@
 // seeds if we end up needing to use them
+const sequelize = require('../config/connection');
+const { Fighter } = require('../models');
+
+const fighterData = require('./sampleFighterData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Fighter.bulkCreate(fighterData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
