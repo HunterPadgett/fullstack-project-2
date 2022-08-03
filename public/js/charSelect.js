a97840954057f14295a7711c6c334a0c4516c3bf
dubsumm <w.b.summerlin@gmail.com>
2022-08-03 14:25:03 -0400

    checkboxes logic fixed

--- public/js/charSelect.js
+++ public/js/charSelect.js
@@ -1,12 +1,24 @@
+formControlCheckbox = [...document.querySelectorAll(".form-control-checkbox")];
+
+formControlCheckbox.forEach((oneByone) => {
+  oneByone.addEventListener('click', function () {
+    formControlCheckbox.forEach((oneByone) => {
+      oneByone.checked = false;
+    });
+    oneByone.checked = true;
+  });
+});
 const fighterHandler = async (event) => {
   event.preventDefault();
 
-  const fighterName = document.getElementById('charNameInput').value.trim();
+  const fighterName = document.getElementById("charNameInput").value.trim();
   console.log(fighterName);
 
-  document.location.replace('/profile');
+  document.location.replace("/profile");
 };
 
 document
-  .getElementById('create-character')
-  .addEventListener('click', fighterHandler);
+  .getElementById("create-character")
+  .addEventListener("click", fighterHandler);
+
+formControlCheckbox = [...document.querySelectorAll(".form-control-checkbox")];
