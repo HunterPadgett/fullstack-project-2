300c4ad0f4e2301bb853ee57301aeec203ea4461
dubsumm <w.b.summerlin@gmail.com>
2022-08-02 22:29:38 -0400

    good frickin luck

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,4 +16,20 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/charSelect', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
+    const user = userData.get({ plain: true });
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
