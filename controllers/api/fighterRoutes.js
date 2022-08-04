1d847da5ec220b8fc7a13fed85f5ddbfd5e39a72
dubsumm <w.b.summerlin@gmail.com>
2022-08-04 14:47:34 -0400

    initial heroku deploy

--- controllers/api/fighterRoutes.js
+++ controllers/api/fighterRoutes.js
@@ -1,28 +1,13 @@
 const router = require('express').Router();
 const { User, Fighter } = require('../../models');
 
-// api/fighter
-
-// router.get('/', async (req, res) => {
-//   // find all categories ✅
-//   // be sure to include its associated Products ✅
-//   try {
-//     const fighterData = await Fighter.findOne({ where: { user_id: req.body.user_id } });
-//     res.status(200).json(fighterData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get('/', async (req, res) => {
-  // find one category by its `id` value ✅
-  // be sure to include its associated Products ✅
   try {
     const fighterData = await Fighter.findOne({where: {user_id: req.session.id}}, {
       include: User
     });
     const user = fighterData.get({ plain: true });
-    // console.log(user.Fighter.fighter_image);
+    console.log(user.Fighter.fighter_image);
     if (!fighterData) {
       res.status(404).json({ message: 'no fighter found'});
       return;
@@ -36,7 +21,6 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
-  console.log(req.body);
   try {
     let tempObj = {
       fighter_image: req.body.fighter_image,
@@ -51,15 +35,17 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/', async (req, res) => {
   try {
-    const fighterData = await Fighter.findByPk(req.body, {
-      where: {
-        id: req.params.id
-      }
-    });
+    let tempObj = {
+      wins: req.body.wins,
+      losses: req.body.losses,
+    };
+    console.log(tempObj);
+    console.log(req.session);
+    
     if (!fighterData[0]) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'Fighter not found' });
       return;
     }
     res.status(200).json(fighterData);
