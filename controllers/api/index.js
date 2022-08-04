773d8a16799ec273e5e4d574b6f78e5daf1ec5e5
Hunter Padgett <gizmokabanjo@gmail.com>
2022-08-04 10:56:59 -0400

    fix route error

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,8 @@ const router = require('express').Router();
 // any require for models routes go here
 const userRoutes = require('./userRoutes.js');
 const fighterRoutes = require('./fighterRoutes.js');
-const styleRoutes = require('./styleRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/fighter', fighterRoutes);
-router.use('/style', styleRoutes);
-
 
 module.exports = router;
