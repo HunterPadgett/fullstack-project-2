c1784756c001baf2d697f2a132c8957bb5abb454
Hunter Padgett <gizmokabanjo@gmail.com>
2022-08-04 20:10:35 -0400

    chart.js working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Create new user with /api/user
 router.post('/', async (req, res) => {
@@ -18,7 +19,9 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username }});
+    const userData = await User.findOne({
+      where: { username: req.body.username }
+    });
 
     if (!userData) {
       res
@@ -57,25 +60,4 @@ router.post('/logout', (req, res) => {
   }
 });
 
-// /api/user/userInfo/:id for showing a specific user in insomnia
-router.get('/userInfo/:id', async (req, res) => {
-  try {
-    const UserInfo = await User.findByPk(req.params.id);
-
-    res.status(200).json(UserInfo);
-    console.log(req.session.logged_in);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/userInfo', async (req, res) => {
-  try {
-    const UserInfo = await User.findAll(req.body);
-
-    res.status(200).json(UserInfo);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 module.exports = router;
